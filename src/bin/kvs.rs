c2a4116052a7e7664573412fc9b5d8aa60a806f0
XFFXFF <1247714429@qq.com>
2020-11-22 22:56:48 +0800

    Accept command line arguments

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, load_yaml};
+use std::process;
+
 fn main() {
-  println!("Hello World!");
+    let yaml = load_yaml!("cli.yml");
+    let m = App::from(yaml).get_matches();  
+
+    match m.subcommand_name() {
+        _ => {
+            eprintln!("unimplemented");
+            process::exit(1);
+        }
+    }
 }
