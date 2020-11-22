2138468c2cbe9f9dbed2da41af405dc020467d78
XFFXFF <1247714429@qq.com>
2020-11-22 20:27:59 +0800

    part1: make the tests compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct KvStore {
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        panic!();
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&self, key: String) {
+        panic!();
     }
 }
