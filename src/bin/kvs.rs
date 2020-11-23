433653cd8146c18d6d209e2f61017af6e27f68fc
XFFXFF <1247714429@qq.com>
2020-11-23 08:31:32 +0800

    do not use yaml and set environment variables

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,13 +1,47 @@
-#[macro_use]
 extern crate clap;
-use clap::{App, load_yaml};
+use clap::{App, Arg, crate_name, crate_version, crate_authors}; 
 use std::process;
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let m = App::from(yaml).get_matches();  
+    let matches = App::new(crate_name!())
+        .about("A kv store")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .subcommand(
+            App::new("get")
+                .about("Gets value according to the key")
+                .arg(
+                    Arg::new("KEY")
+                        .required(true)
+                        .index(1)
+                )
+            )
+        .subcommand(
+            App::new("set")
+                .about("Sets key-value pair")
+                .arg(
+                    Arg::new("KEY")
+                        .required(true)
+                        .index(1)
+                )
+                .arg(
+                    Arg::new("VALUE")
+                        .required(true)
+                        .index(2)
+                ),
+        )
+        .subcommand(
+            App::new("rm")
+                .about("Remove key-value pair according to the key")
+                .arg(
+                    Arg::new("KEY")
+                        .required(true)
+                        .index(1)
+                )
+        )
+        .get_matches();
 
-    match m.subcommand_name() {
+    match matches.subcommand_name() {
         _ => {
             eprintln!("unimplemented");
             process::exit(1);
