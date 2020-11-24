215439777b4ada3efacdcc382b36e528c0f7de62
XFFXFF <1247714429@qq.com>
2020-11-24 08:19:44 +0800

    store values in memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,29 @@
+use std::collections::HashMap;
+
 pub struct KvStore {
+    hash_map: HashMap<String, String>,
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        panic!();
+        let hash_map = HashMap::new();
+        KvStore{hash_map}
     }
 
-    pub fn set(&self, key: String, value: String) {
-        panic!();
+    pub fn set(&mut self, key: String, value: String) {
+        self.hash_map.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        panic!();
+        match self.hash_map.get(&key) {
+            Some(value) => {
+                Some(value.to_owned())
+            }
+            None => None
+        }
     }
 
-    pub fn remove(&self, key: String) {
-        panic!();
+    pub fn remove(&mut self, key: String) {
+        self.hash_map.remove(&key);
     }
 }
