ed73f5a7472fd1837bc77431f2f275586b3d6b72
XFFXFF <1247714429@qq.com>
2020-11-24 09:01:53 +0800

    simple doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,43 @@ pub struct KvStore {
 }
 
 impl KvStore {
+    /// Returns a new key value store
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use kvs::KvStore;
+    /// let kvs = KvStore::new();
+    /// ```
     pub fn new() -> KvStore {
         let hash_map = HashMap::new();
         KvStore{hash_map}
     }
 
+    /// Store one key value pair
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use kvs::KvStore;
+    /// let kvs = KvStore::new();
+    /// 
+    /// kvs.set("key1".to_owned(), "value1".to_owned());
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.hash_map.insert(key, value);
     }
 
+    /// Get value according to the key
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use kvs::KvStore;
+    /// let kvs = KvStore::new();
+    /// 
+    /// kvs.get("key1".to_owned());
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
         match self.hash_map.get(&key) {
             Some(value) => {
@@ -23,6 +51,16 @@ impl KvStore {
         }
     }
 
+    /// Remove the key value pair according to the key
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use kvs::KvStore;
+    /// let kvs = KvStore::new();
+    /// 
+    /// kvs.remove("key1".to_owned());
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.hash_map.remove(&key);
     }
