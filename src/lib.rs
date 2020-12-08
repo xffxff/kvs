da4b467017be41b746ba099e5a6e5ac45c2dda03
XFFXFF <1247714429@qq.com>
2020-12-08 08:18:14 +0800

    project setup

--- src/lib.rs
+++ src/lib.rs
@@ -180,3 +180,15 @@ impl KvStore {
         Ok((index, log_count))
     }
 }
+
+/// Define the storage interface
+pub trait KvsEngine {
+    /// Set the value of a string key to a string
+    fn set(&mut self, key: String, value: String) -> Result<()>;
+
+    /// Get the string value of a string key. If the key does not exist, return None
+    fn get(&mut self, key: String) -> Result<Option<String>>;
+
+    /// Remove a given string key
+    fn remove(&mut self, key: String) -> Result<()>;
+}
