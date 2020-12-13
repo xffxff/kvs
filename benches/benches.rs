49c8df1be1c332ed3b4e913bcd953feb956c7979
XFFXFF <1247714429@qq.com>
2020-12-13 17:27:12 +0800

    kvs read and sled read benchmark

--- benches/benches.rs
+++ benches/benches.rs
@@ -47,5 +47,50 @@ pub fn sled_write(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, kvs_write, sled_write);
+pub fn kvs_read(c: &mut Criterion) {
+    let temp_dir = TempDir::new().expect("unabble to create temporary working directory");
+    let mut store = KvStore::open(temp_dir.path()).unwrap();
+
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
+    let mut keys: Vec<String> = Vec::new();
+    for _ in 0..100 {
+        let rand_key = random_string(&mut rng);
+        let rand_value = random_string(&mut rng);
+        keys.push(rand_key.clone());
+        store.set(rand_key, rand_value).unwrap()
+    }
+
+    c.bench_function("kvs read", |b| {
+        b.iter(|| {
+            for key in &keys {
+                store.get(key.to_owned()).unwrap();
+            }
+        });
+    });
+}
+
+pub fn sled_read(c: &mut Criterion) {
+    let temp_dir = TempDir::new().expect("unabble to create temporary working directory");
+    let mut store = SledKVStore::open(temp_dir.path()).unwrap();
+
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
+    let mut keys: Vec<String> = Vec::new();
+    for _ in 0..100 {
+        let rand_key = random_string(&mut rng);
+        let rand_value = random_string(&mut rng);
+        keys.push(rand_key.clone());
+        store.set(rand_key, rand_value).unwrap()
+    }
+
+    c.bench_function("sled read", |b| {
+        b.iter(|| {
+            for key in &keys {
+                store.get(key.to_owned()).unwrap();
+            }
+        });
+    });
+}
+
+// criterion_group!(benches, kvs_write, sled_write, kvs_read);
+criterion_group!(benches, kvs_read, sled_read);
 criterion_main!(benches);
