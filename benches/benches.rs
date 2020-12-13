8326c12fad6c7ff4cd9dca93484bb2ce70b536fd
XFFXFF <1247714429@qq.com>
2020-12-13 16:53:32 +0800

    update write benchmark

--- benches/benches.rs
+++ benches/benches.rs
@@ -6,43 +6,46 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{Rng, SeedableRng};
 use rand::distributions::Alphanumeric;
 
-pub fn kvs_write(c: &mut Criterion) {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let mut store = KvStore::open(temp_dir.path()).unwrap();
+fn random_string(rng: &mut impl rand::RngCore) -> String {
+    let size = rng.gen_range(1, 100000);
+    let rand_string = rng
+        .sample_iter(&Alphanumeric)
+        .take(size)
+        .collect();
+    return rand_string;
+}
 
+pub fn kvs_write(c: &mut Criterion) {
     c.bench_function("kvs write", |b| {
         b.iter(|| {
+            let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+            let mut store = KvStore::open(temp_dir.path()).unwrap();
+
             let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
             for _ in 0..100 {
-                let key_size = rng.gen_range(1, 10);
-                let rand_key: String = rng.clone()
-                    .sample_iter(&Alphanumeric)
-                    .take(key_size)
-                    .collect();
-                store.set(rand_key, "value1".to_owned()).unwrap();
+                let rand_key = random_string(&mut rng);
+                let rand_value = random_string(&mut rng);
+                store.set(rand_key, rand_value).unwrap();
             }
         });
     });
 }
 
 pub fn sled_write(c: &mut Criterion) {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let mut store = SledKVStore::open(temp_dir.path()).unwrap();
-
     c.bench_function("sled write", |b| {
         b.iter(|| {
+            let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+            let mut store = SledKVStore::open(temp_dir.path()).unwrap();
+
             let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
             for _ in 0..100 {
-                let key_size = rng.gen_range(1, 10);
-                let rand_key: String = rng.clone()
-                    .sample_iter(&Alphanumeric)
-                    .take(key_size)
-                    .collect();
-                store.set(rand_key, "value1".to_owned()).unwrap();
+                let rand_key = random_string(&mut rng);
+                let rand_value = random_string(&mut rng);
+                store.set(rand_key, rand_value).unwrap();
             }
         });
     });
 }
 
-criterion_group!(benches, sled_write, kvs_write);
+criterion_group!(benches, kvs_write, sled_write);
 criterion_main!(benches);
