20180690fdedc25cfab5b412841924273677c420
zhoufan <1247714429@qq.com>
2020-12-26 14:28:57 +0800

    add client pool

--- benches/thread_pool.rs
+++ benches/thread_pool.rs
@@ -28,22 +28,27 @@ pub fn write_queued_kvstore(c: &mut Criterion) {
             let pool = RayonThreadPool::new(*num as u32).unwrap();
             let (tx, rx) = mpsc::channel();
             let server = KvsServer::new(store, pool, Some(rx));
-            let mut rng = rand::thread_rng();
-            let port = rng.gen_range(4000, 7000);
+            let port = 4000;
             let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
-            thread::spawn(move || {
+            let handle = thread::spawn(move || {
                 server.run(addr).unwrap();
             });
+            let client_pool = RayonThreadPool::new(8).unwrap();
             thread::sleep(Duration::from_millis(5));
             b.iter(|| {
-                for i in 0..1000 {
-                    let mut client = KvsClient::new(&addr).unwrap();
-                    client
-                        .set(format!("key{}", i), "value1".to_owned())
-                        .unwrap();
+                for i in 0..8 {
+                    client_pool.spawn(move || {
+                        for j in 0..100 {
+                            let mut client = KvsClient::new(&addr).unwrap();
+                            client
+                                .set(format!("key{}", i * 100 + j), "value1".to_owned())
+                                .unwrap();
+                        }
+                    });
                 }
             });
             tx.send(()).unwrap();
+            handle.join().unwrap();
         },
         inputs,
     );
