2fc916620a87bc816c86c91145b330047aea8cc4
XFFXFF <1247714429@qq.com>
2020-12-29 21:29:21 +0800

    add benchmark for rayon

--- benches/thread_pool.rs
+++ benches/thread_pool.rs
@@ -70,7 +70,7 @@ pub fn read_queued_kvstore(c: &mut Criterion) {
             });
             thread::sleep(Duration::from_millis(10));
             {
-                let client_num = 1000;
+                let client_num = 8;
                 let client_pool = SharedQueueThreadPool::new(client_num).unwrap();
 
                 let (sender, receiver) = mpsc::channel();
@@ -92,7 +92,106 @@ pub fn read_queued_kvstore(c: &mut Criterion) {
                     for i in 0..1000 {
                         let sender = mpsc::Sender::clone(&sender);
                         client_pool.spawn(move || {
-                            println!("get {}", i);
+                            let mut client = KvsClient::new(&addr).unwrap();
+                            let response = client.get(format!("key{}", i)).unwrap();
+                            if let Response::Ok(option) = response {
+                                assert_eq!(option, Some("value".to_string()));
+                            }
+                            sender.send(()).unwrap();
+                        });
+                    }
+                    drop(sender);
+                    for _ in receiver {}
+                });
+            }
+            tx.send(()).unwrap();
+            handle.join().unwrap();
+        },
+        inputs,
+    );
+}
+
+pub fn write_rayon_kvstore(c: &mut Criterion) {
+    let inputs = &[1, 2, 4, 8, 16];
+    c.bench_function_over_inputs(
+        "write queued kvstore",
+        |b, &num| {
+            let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+            let store = KvStore::open(temp_dir.path()).unwrap();
+            let pool = RayonThreadPool::new(*num as u32).unwrap();
+            let (tx, rx) = mpsc::channel();
+            let server = KvsServer::new(store, pool, Some(rx));
+            let port = 4000;
+            let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+            let handle = thread::spawn(move || {
+                server.run(addr).unwrap();
+            });
+            thread::sleep(Duration::from_millis(10));
+            {
+                let client_num = 8;
+                let client_pool = SharedQueueThreadPool::new(client_num).unwrap();
+                b.iter(|| {
+                    let (sender, receiver) = mpsc::channel();
+                    for i in 0..1000 {
+                        let sender = mpsc::Sender::clone(&sender);
+                        client_pool.spawn(move || {
+                            let mut client = KvsClient::new(&addr).unwrap();
+                            client
+                                .set(format!("key{}", i), "value1".to_owned())
+                                .unwrap();
+                            sender.send(()).unwrap();
+                        });
+                    }
+                    drop(sender);
+                    for _ in receiver {}
+                });
+            }
+            tx.send(()).unwrap();
+            handle.join().unwrap();
+        },
+        inputs,
+    );
+}
+
+pub fn read_rayon_kvstore(c: &mut Criterion) {
+    let inputs = &[1, 2, 4, 8, 16];
+    c.bench_function_over_inputs(
+        "write queued kvstore",
+        |b, &num| {
+            let temp_dir = TempDir::new().expect("unable to create temporary working directory");
+            let store = KvStore::open(temp_dir.path()).unwrap();
+            let pool = RayonThreadPool::new(*num as u32).unwrap();
+            let (tx, rx) = mpsc::channel();
+            let server = KvsServer::new(store, pool, Some(rx));
+            let port = 4000;
+            let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+            let handle = thread::spawn(move || {
+                server.run(addr).unwrap();
+            });
+            thread::sleep(Duration::from_millis(10));
+            {
+                let client_num = 8;
+                let client_pool = SharedQueueThreadPool::new(client_num).unwrap();
+
+                let (sender, receiver) = mpsc::channel();
+                for i in 0..1000 {
+                    let sender = mpsc::Sender::clone(&sender);
+                    client_pool.spawn(move || {
+                        let mut client = KvsClient::new(&addr).unwrap();
+                        client
+                            .set(format!("key{}", i), "value".to_owned())
+                            .unwrap();
+                        sender.send(()).unwrap();
+                    });
+                }
+                drop(sender);
+                for _ in receiver {}
+
+                b.iter(|| {
+                    let (sender, receiver) = mpsc::channel();
+                    for i in 0..1000 {
+                        let sender = mpsc::Sender::clone(&sender);
+                        client_pool.spawn(move || {
                             let mut client = KvsClient::new(&addr).unwrap();
                             let response = client.get(format!("key{}", i)).unwrap();
                             if let Response::Ok(option) = response {
@@ -114,5 +213,5 @@ pub fn read_queued_kvstore(c: &mut Criterion) {
 
 
 
-criterion_group!(benches, read_queued_kvstore);
+criterion_group!(benches, read_rayon_kvstore);
 criterion_main!(benches);
