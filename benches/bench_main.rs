9221def1c841a1b9f30b20239a5a9fbe1c50159e
XFFXFF <1247714429@qq.com>
2021-01-02 10:30:34 +0800

    uncomment some benchmarks

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -3,10 +3,10 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main!(
-    // benchmarks::engine::write,
-    // benchmarks::engine::read,
-    // benchmarks::thread_pool::write_queued,
-    // benchmarks::thread_pool::write_rayon,
+    benchmarks::engine::write,
+    benchmarks::engine::read,
+    benchmarks::thread_pool::write_queued,
+    benchmarks::thread_pool::write_rayon,
     benchmarks::thread_pool::read_queued,
-    // benchmarks::thread_pool::read_rayon,
+    benchmarks::thread_pool::read_rayon,
 );
